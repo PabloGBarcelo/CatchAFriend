0b145b53d6d8f26c16ce75309573bc458e1cab29
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-12-14 19:37:20 +0100

    deploying

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  backFacebookLogin: "http://localhost:3000/auth/facebook",
+  backFacebookLogin: "http://catchafriend.herokuapp.com/auth/facebook",
   BASE_DOMAIN: '',
-  imageUpload: 'http://localhost:3000/api/uploadPhoto'
+  imageUpload: 'http://catchafriend.herokuapp.com/api/uploadPhoto'
 
 };
